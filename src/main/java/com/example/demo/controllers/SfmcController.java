ae1a722ebeac416462c26f7bc1ba48fd23369e37
Devesh <devesh.pandey@walmart.com>
2025-06-17 18:17:38 +0530

    added request logger

--- src/main/java/com/example/demo/controllers/SfmcController.java
+++ src/main/java/com/example/demo/controllers/SfmcController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 import com.example.demo.model.ExecuteResponse;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -7,20 +8,23 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api")
 public class SfmcController {
     @PostMapping(value = "/execute")
-    public ResponseEntity<ExecuteResponse> execute() {
+    public ResponseEntity<ExecuteResponse> execute(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC execute endpoint");
+        System.out.println("Received request: " + requestBody.toString());
         ExecuteResponse executeResponse = new ExecuteResponse();
         executeResponse.setFoundSignupDate("2023-10-01T00:00:00Z");
         return ResponseEntity.ok(executeResponse);
     }
     @PostMapping(value = "/save")
-    public ResponseEntity<String> save() {
+    public ResponseEntity<String> save(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC save endpoint");
+        System.out.println("Received request: " + requestBody.toString());
         return ResponseEntity.ok("Saved");
     }
     @PostMapping(value = "/publish")
-    public ResponseEntity<String> publish() {
+    public ResponseEntity<String> publish(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC publish endpoint");
+        System.out.println("Received request: " + requestBody.toString());
         return ResponseEntity.ok("Published");
     }
 }
