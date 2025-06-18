7207af7967f5284e9e69a33bb245a2c1fb18a415
Devesh <devesh.pandey@walmart.com>
2025-06-18 12:16:30 +0530

    removed outArguments from response

--- src/main/java/com/example/demo/controllers/SfmcController.java
+++ src/main/java/com/example/demo/controllers/SfmcController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 import com.example.demo.model.ExecuteResponse;
-import com.example.demo.model.OutArguments;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +14,6 @@ public class SfmcController {
         System.out.println("This is the SFMC execute endpoint");
         System.out.println("Received request: " + requestBody.toString());
         ExecuteResponse executeResponse = new ExecuteResponse();
-        OutArguments outArguments = new OutArguments();
         ArrayList<ExecuteResponse> outArgumentsArrayList = new ArrayList<>();
         if (requestBody.has("inArguments") && requestBody.get("inArguments").isArray()) {
             JsonNode inArguments = requestBody.get("inArguments").get(0); // Get the first element
@@ -24,9 +22,7 @@ public class SfmcController {
                 if (message.equals("unknownMessage")) {
                     executeResponse.setFoundSignupDate("2024-10-01T00:00:00Z");
                     executeResponse.setAlternateSignupDate("2025-10-01T00:00:00Z");
-                    outArgumentsArrayList.add(executeResponse);
-                    outArguments.setOutArguments(outArgumentsArrayList);
-                    return ResponseEntity.ok(outArguments);
+                    return ResponseEntity.ok(executeResponse);
                 } else{
                     executeResponse.setFoundSignupDate("2023-10-01T00:00:00Z");
                     executeResponse.setAlternateSignupDate("2025-10-01T00:00:00Z");
@@ -41,9 +37,7 @@ public class SfmcController {
             executeResponse.setAlternateSignupDate("2025-10-01T00:00:00Z");
         }
         System.out.println("ExecuteResponse: " + executeResponse.getFoundSignupDate());
-        outArgumentsArrayList.add(executeResponse);
-        outArguments.setOutArguments(outArgumentsArrayList);
-        return ResponseEntity.ok(outArguments);
+        return ResponseEntity.ok(executeResponse);
     }
     @PostMapping(value = "/save")
     public ResponseEntity<String> save(@RequestBody JsonNode requestBody) {
