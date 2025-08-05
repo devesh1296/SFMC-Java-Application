bd68e5dc8700d45a397a659543cccc174f57a4ee
Devesh <devesh.pandey@walmart.com>
2025-08-05 11:46:28 +0530

    commented logic for runtime schema

--- src/main/resources/static/js/customActivity.js
+++ src/main/resources/static/js/customActivity.js
@@ -103,34 +103,34 @@ define(["postmonger"], function (Postmonger) {
         const form = document.createElement("form");
         form.id = "dynamicForm";
 
-        schema.forEach((field) => {
-            const label = document.createElement("label");
-            label.innerText = field.name;
-            label.style.display = "block";
-
-            let inputElement;
-
-            // 1. Dropdown for FirstName and CID
-            if (["emailAddress", "CID", "FirstName"].includes(field.name)) {
-                inputElement = document.createElement("select");
-                inputElement.id = field.name;
-
-                const defaultOption = document.createElement("option");
-                defaultOption.value = "";
-                defaultOption.text = `-- Select ${field.name} --`;
-                inputElement.appendChild(defaultOption);
-
-                const dynamicOption = document.createElement("option");
-                dynamicOption.value = `{{${field.key}}}`;
-                dynamicOption.text = field.name;
-                inputElement.appendChild(dynamicOption);
-            }
-            if (inputElement) {
-                inputElement.style.marginBottom = "10px";
-                form.appendChild(label);
-                form.appendChild(inputElement);
-            }
-        });
+        // schema.forEach((field) => {
+        //     const label = document.createElement("label");
+        //     label.innerText = field.name;
+        //     label.style.display = "block";
+        //
+        //     let inputElement;
+        //
+        //     // 1. Dropdown for FirstName and CID
+        //     if (["emailAddress", "CID", "FirstName"].includes(field.name)) {
+        //         inputElement = document.createElement("select");
+        //         inputElement.id = field.name;
+        //
+        //         const defaultOption = document.createElement("option");
+        //         defaultOption.value = "";
+        //         defaultOption.text = `-- Select ${field.name} --`;
+        //         inputElement.appendChild(defaultOption);
+        //
+        //         const dynamicOption = document.createElement("option");
+        //         dynamicOption.value = `{{${field.key}}}`;
+        //         dynamicOption.text = field.name;
+        //         inputElement.appendChild(dynamicOption);
+        //     }
+        //     if (inputElement) {
+        //         inputElement.style.marginBottom = "10px";
+        //         form.appendChild(label);
+        //         form.appendChild(inputElement);
+        //     }
+        // });
         // 2. Always add Platform dropdown
         const platformLabel = document.createElement("label");
         platformLabel.innerText = "Platform";
