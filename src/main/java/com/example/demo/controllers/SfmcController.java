443ca4348378a982e6118f4d75a7894c0561eb2c
Devesh Pandey <pandeydevesh1296@gmail.com>
2025-08-22 16:26:57 +0530

    Added String as RequestBody type

--- src/main/java/com/example/demo/controllers/SfmcController.java
+++ src/main/java/com/example/demo/controllers/SfmcController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controllers;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -7,26 +6,26 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api")
 public class SfmcController {
     @PostMapping(value = "/execute", consumes = {"application/json", "application/jwt"})
-    public void execute(@RequestBody JsonNode requestBody) {
+    public void execute(@RequestBody String requestBody) {
         System.out.println("This is the SFMC execute endpoint");
-        System.out.println("Received request: " + requestBody.toString());
+        System.out.println("Received request: " + requestBody);
     }
     @PostMapping(value = "/save", consumes = {"application/json", "application/jwt"})
-    public ResponseEntity<String> save(@RequestBody JsonNode requestBody) {
+    public ResponseEntity<String> save(@RequestBody String requestBody) {
         System.out.println("This is the SFMC save endpoint");
-        System.out.println("Received request: " + requestBody.toString());
+        System.out.println("Received request: " + requestBody);
         return ResponseEntity.ok("Saved");
     }
     @PostMapping(value = "/publish", consumes = {"application/json", "application/jwt"})
-    public ResponseEntity<String> publish(@RequestBody JsonNode requestBody) {
+    public ResponseEntity<String> publish(@RequestBody String requestBody) {
         System.out.println("This is the SFMC publish endpoint");
-        System.out.println("Received request: " + requestBody.toString());
+        System.out.println("Received request: " + requestBody);
         return ResponseEntity.ok("Published");
     }
     @PostMapping(value = "/validate", consumes = {"application/json", "application/jwt"})
-    public ResponseEntity<String> validate(@RequestBody JsonNode requestBody) {
+    public ResponseEntity<String> validate(@RequestBody String requestBody) {
         System.out.println("This is the SFMC validate endpoint");
-        System.out.println("Received request: " + requestBody.toString());
+        System.out.println("Received request: " + requestBody);
         return ResponseEntity.ok("Validated");
     }
 }
