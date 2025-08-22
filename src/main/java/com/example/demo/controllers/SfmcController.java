59f5293e6022ba1bab0950178ff58c10ab9a0a19
Devesh Pandey <pandeydevesh1296@gmail.com>
2025-08-22 16:24:50 +0530

    Added Consumes

--- src/main/java/com/example/demo/controllers/SfmcController.java
+++ src/main/java/com/example/demo/controllers/SfmcController.java
@@ -6,24 +6,24 @@ import org.springframework.http.ResponseEntity;
 @RestController
 @RequestMapping("/api")
 public class SfmcController {
-    @PostMapping(value = "/execute", consumes = "application/jwt")
+    @PostMapping(value = "/execute", consumes = {"application/json", "application/jwt"})
     public void execute(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC execute endpoint");
         System.out.println("Received request: " + requestBody.toString());
     }
-    @PostMapping(value = "/save", consumes = "application/jwt")
+    @PostMapping(value = "/save", consumes = {"application/json", "application/jwt"})
     public ResponseEntity<String> save(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC save endpoint");
         System.out.println("Received request: " + requestBody.toString());
         return ResponseEntity.ok("Saved");
     }
-    @PostMapping(value = "/publish", consumes = "application/jwt")
+    @PostMapping(value = "/publish", consumes = {"application/json", "application/jwt"})
     public ResponseEntity<String> publish(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC publish endpoint");
         System.out.println("Received request: " + requestBody.toString());
         return ResponseEntity.ok("Published");
     }
-    @PostMapping(value = "/validate", consumes = "application/jwt")
+    @PostMapping(value = "/validate", consumes = {"application/json", "application/jwt"})
     public ResponseEntity<String> validate(@RequestBody JsonNode requestBody) {
         System.out.println("This is the SFMC validate endpoint");
         System.out.println("Received request: " + requestBody.toString());
